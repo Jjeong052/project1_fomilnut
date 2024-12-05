de6f44b893aac196d87a6cc9129b5f4001c2da40
Jjeong052 <br_jjeong@naver.com>
2024-12-05 09:19:53 +0900

    최초 업로드

---
+++ js/common.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+  AOS.init();
+
+  $(".depth2").hide();
+  $(".gnb > li").hover(function(){
+    $(this).children(".depth2").stop().fadeToggle();
+  });
+
+  var mv = new Swiper(".mv", {
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+
+  var shopSlide = new Swiper(".shop-slide", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    
+  });
+
+
+
+  
+});
